5abe8c2d0ac0be302a68faf80ef7d3d520a95be2
jbjoannic <jb.joannic@etu.emse.fr>
2021-10-09 12:17:00 +0200

    tests matin sans internet

--- compteur.py
+++ compteur.py
@@ -74,119 +74,130 @@ fr=1
 originale=cv2.cvtColor(originale, cv2.COLOR_BGR2GRAY)       #noir et blanc, essayer COLOR_BGR2HSV
 originale=cv2.GaussianBlur(originale, (kernel_blur, kernel_blur), 0)        #mettre le flou  
 kernel_dilate=np.ones((5, 5), np.uint8) #par défaut en float64, la le type en int8
-kernel_morph=disk(30)
-plt.imshow(kernel_morph)
-plt.title("SEmorph")
-plt.show()
+kernel_morphcl=disk(50)
+kernel_morphop=disk(15)
+#plt.imshow(kernel_morph)
+# plt.title("SEmorph")
+# plt.show()
+div_frame=2
+ind=0
 
 while True:
-        
+    
     ret, framegd=cap.read()
-    
-    # if fr == 30:
-    #     originale=cv2.cvtColor(framegd, cv2.COLOR_BGR2GRAY)       #noir et blanc, essayer COLOR_BGR2HSV
-    #     originale=cv2.GaussianBlur(originale, (kernel_blur, kernel_blur), 0)
-        
-    tickmark=cv2.getTickCount()  #return le nb de tick depuis un moment precis (ex, le demarrage du kernel)
-    frame=framegd #[50:250,200:400]  #meme redimensionnement
-    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-    # plt.imshow(gray)
-    # plt.title("gray")
-    # plt.show()
-    
-    
-    gray=cv2.GaussianBlur(gray, (kernel_blur, kernel_blur), 0)
-    # plt.imshow(gray)
-    # plt.title("flou")
-    # plt.show()
-    
-    grays=cv2.threshold(gray, seuil, 255, cv2.THRESH_BINARY)[1]
-    # plt.imshow(grays)
-    # plt.title("seuil simple")
-    # plt.show()
-    
-    
-    mask=cv2.absdiff(originale, gray)  #difference absolue
-    # plt.imshow(mask)
-    # plt.title("difference")
-    # plt.show()
-    
-    mask=cv2.threshold(mask, seuil, 255, cv2.THRESH_BINARY)[1] #retourne un threshold
-    # plt.imshow(mask)
-    # plt.title("seuil diff {0}".format(fr))
-    # plt.show()
-    
-    mask1=cv2.erode(mask,kernel_dilate, iterations=10) #50 erosions pour enlever les bails
-    # plt.imshow(mask)
-    # plt.title("erodé")
-    # plt.show()
-    mask1=cv2.dilate(mask1, kernel_dilate, iterations=10) #dilatation
-    # plt.imshow(mask)
-    # plt.title("erodé")
-    # plt.show()
-    
-    mask2=cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_morph)
-    # plt.imshow(mask2)
-    # plt.title("ouvert diff {0}".format(fr))
-    # plt.show()
-    mask2=cv2.morphologyEx(mask2, cv2.MORPH_CLOSE, kernel_morph)
-    # plt.imshow(mask2)
-    # plt.title("fermé diff {0}".format(fr))
-    # plt.show()
-    
-    contours, nada=cv2.findContours(mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)       #trouver contours, plusieurs fermés
-    frame_contour=frame.copy()
-    fr+=1
-    for c in contours:
-        cv2.drawContours(frame_contour, [c], 0, (0, 255, 0), 5)     #dessiner les contours
-        if cv2.contourArea(c)<surface:      #afficher que les rectangles avec une surface supérieure à celle rentrée
-            continue
-        x, y, w, h=cv2.boundingRect(c)  #trace les rectangles (le + grand ds le contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-        centre.append((int(x+w/2),int(y+h/2)))
-        
-    #print(Etat)
-    # for i in range (0,5): #Mise à jour de Etat et détection de la fin d'un passage
-    #     if Etat[i][0]!=(-1,-1):
-    #         indmin=trouvemin(centre,Etat[i][-1])
-    #         if indmin==-1:
-    #             if len(Etat[i])>3:
-    #                 p=p+sens(Etat[i])   #le nb de personnes est fct du sens
-    #                 Etat[i]=[(-1,-1)]
-    #         else:
-    #             Etat[i].append(centre[indmin])
-    #             del centre[indmin]
-                
-    # if len(centre)!=0: #Introduction d'un nouvel intrus
-    #     n=-1
-    #     for nouveau in centre:
-    #         n+=1
-    #         i=0
-    #         while Etat[i][0]!=(-1,-1):
-    #             i+=1
-    #         Etat[i][0]=nouveau
-    #         del centre[n]
+    if ind%div_frame==0: 
+        # if fr == 30:
+        #     originale=cv2.cvtColor(framegd, cv2.COLOR_BGR2GRAY)       #noir et blanc, essayer COLOR_BGR2HSV
+        #     originale=cv2.GaussianBlur(originale, (kernel_blur, kernel_blur), 0)
             
-    # for i in range(0,5): # Dessin de la trace
-    #     if Etat[i][0]!=(-1,-1):
-    #         n=len(Etat[i])
-    #         for j in range(0,n-1):
-    #             cv2.line(frame, Etat[i][j], Etat[i][j+1], (0, 255, 0), 10)
-                
-    #originale=gray
-    # fps=cv2.getTickFrequency()/(cv2.getTickCount()-tickmark)
-    # if mfps>fps:
-    #     mfps=fps
+        tickmark=cv2.getTickCount()  #return le nb de tick depuis un moment precis (ex, le demarrage du kernel)
+        frame=framegd #[50:250,200:400]  #meme redimensionnement
+        gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+        # plt.imshow(gray)
+        # plt.title("gray")
+        # plt.show()
         
-    ##Affichage
-    
-    #cv2.putText(frame, "FPS: {:05.2f} [o|l]seuil: {:d}  [p|m]blur: {:d}  [i|k]surface: {:d} p= {:d}".format(mfps, seuil, kernel_blur, surface,p), (10, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), 2)
-    cv2.imshow("frame", frame)
-    cv2.imshow("seuildiff", mask)
-    #cv2.imshow("contour", frame_contour)
-    cv2.imshow("morphfermé", mask2)
-    intrus=0
-    
+        
+        gray=cv2.GaussianBlur(gray, (kernel_blur, kernel_blur), 0)
+        # plt.imshow(gray)
+        # plt.title("flou")
+        # plt.show()
+        
+        grays=cv2.threshold(gray, seuil, 255, cv2.THRESH_BINARY)[1]
+        # plt.imshow(grays)
+        # plt.title("seuil simple")
+        # plt.show()
+        
+        
+        mask=cv2.absdiff(originale, gray)  #difference absolue
+        # plt.imshow(mask)
+        # plt.title("difference")
+        # plt.show()
+        
+        mask=cv2.threshold(mask, seuil, 255, cv2.THRESH_BINARY)[1] #retourne un threshold
+        # plt.imshow(mask)
+        # plt.title("seuil diff {0}".format(fr))
+        # plt.show()
+        
+        mask1=cv2.erode(mask,kernel_dilate, iterations=30) #50 erosions pour enlever les bails
+        # plt.imshow(mask)
+        # plt.title("erodé")
+        # plt.show()
+        mask1=cv2.dilate(mask1, kernel_dilate, iterations=30) #dilatation
+        # plt.imshow(mask)
+        # plt.title("erodé")
+        # plt.show()
+        
+        mask2=cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_morphop)
+        # plt.imshow(mask2)
+        # plt.title("ouvert diff {0}".format(fr))
+        # plt.show()
+        mask2=cv2.morphologyEx(mask2, cv2.MORPH_CLOSE, kernel_morphcl)
+        # plt.imshow(mask2)
+        # plt.title("fermé diff {0}".format(fr))
+        # plt.show()
+        
+        
+        
+        mask3=cv2.erode(mask,kernel_dilate, iterations=30)
+        mask3=cv2.dilate(mask3,kernel_dilate, iterations=30)
+        mask3=cv2.morphologyEx(mask3, cv2.MORPH_CLOSE, kernel_morphcl)
+        #mask3=cv2.morphologyEx(mask3, cv2.MORPH_OPEN, kernel_morphop)
+        
+        
+        contours, nada=cv2.findContours(mask3, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)       #trouver contours, plusieurs fermés
+        frame_contour=frame.copy()
+        fr+=1
+        for c in contours:
+            cv2.drawContours(frame_contour, [c], 0, (0, 255, 0), 5)     #dessiner les contours
+            if cv2.contourArea(c)<surface:      #afficher que les rectangles avec une surface supérieure à celle rentrée
+                continue
+            x, y, w, h=cv2.boundingRect(c)  #trace les rectangles (le + grand ds le contour)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+            centre.append((int(x+w/2),int(y+h/2)))
+            
+        #print(Etat)
+        # for i in range (0,5): #Mise à jour de Etat et détection de la fin d'un passage
+        #     if Etat[i][0]!=(-1,-1):
+        #         indmin=trouvemin(centre,Etat[i][-1])
+        #         if indmin==-1:
+        #             if len(Etat[i])>3:
+        #                 p=p+sens(Etat[i])   #le nb de personnes est fct du sens
+        #                 Etat[i]=[(-1,-1)]
+        #         else:
+        #             Etat[i].append(centre[indmin])
+        #             del centre[indmin]
+                    
+        # if len(centre)!=0: #Introduction d'un nouvel intrus
+        #     n=-1
+        #     for nouveau in centre:
+        #         n+=1
+        #         i=0
+        #         while Etat[i][0]!=(-1,-1):
+        #             i+=1
+        #         Etat[i][0]=nouveau
+        #         del centre[n]
+                
+        # for i in range(0,5): # Dessin de la trace
+        #     if Etat[i][0]!=(-1,-1):
+        #         n=len(Etat[i])
+        #         for j in range(0,n-1):
+        #             cv2.line(frame, Etat[i][j], Etat[i][j+1], (0, 255, 0), 10)
+                    
+        #originale=gray
+        # fps=cv2.getTickFrequency()/(cv2.getTickCount()-tickmark)
+        # if mfps>fps:
+        #     mfps=fps
+            
+        ##Affichage
+        
+        #cv2.putText(frame, "FPS: {:05.2f} [o|l]seuil: {:d}  [p|m]blur: {:d}  [i|k]surface: {:d} p= {:d}".format(mfps, seuil, kernel_blur, surface,p), (10, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), 2)
+        cv2.imshow("frame", frame)
+        cv2.imshow("seuildiff", mask)
+        #cv2.imshow("contour", frame_contour)
+        cv2.imshow("morphfermé", mask3)
+        intrus=0
+    ind+=1
     ##Boutons de modification des paramètres
     
     key=cv2.waitKey(20)&0xFF
